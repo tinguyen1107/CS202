8e7ab772cce88a05acfb20c36bf864043d039dd0
HuynhSang <sangnguyen3072002@gmail.com>
2021-12-03 13:09:04 +0700

    [Implement] Animal

--- Animal/CAnimal.cpp
+++ Animal/CAnimal.cpp
@@ -1,1 +1,26 @@
 #include "CAnimal.h"
+
+#include <iostream>
+
+using namespace std;
+
+/*-------------------------------------------------------------*/
+/* CLASS Animal */
+/*-------------------------------------------------------------*/
+CAnimal::CAnimal(int x, int y) : mX(x), mY(y) {}
+
+void CAnimal::move(int x, int y) { mX += x; mY += y; }
+
+/*-------------------------------------------------------------*/
+/* CLASS BIRD */
+/*-------------------------------------------------------------*/
+CBird::CBird(int x, int y) : CAnimal(x, y) {};
+
+void CBird::tell() { cout << "Bird tell" << endl; }
+
+/*-------------------------------------------------------------*/
+/* CLASS DINAUSOR */
+/*-------------------------------------------------------------*/
+CDinausor::CDinausor(int x, int y) : CAnimal(x, y) {};
+
+void CDinausor::tell() { cout << "Bird tell" << endl; }
--- Animal/CAnimal.h
+++ Animal/CAnimal.h
@@ -1,25 +1,25 @@
 #ifndef CAnimal_h
 #define CAnimal_h
 
-// #include "../Base/Base.h"
-
 class CAnimal {
 private:
     int mX, mY;
 public:
-    virtual void move(int, int) = 0;
+    CAnimal(int x, int y);
+    void move(int, int);
     virtual void tell() = 0;
 };
 
 class CBird : CAnimal {
-    void move(int, int);
+public:
+    CBird(int x, int y);
     void tell();
 };
 
 class CDinausor : CAnimal {
-    void move(int, int);
+public:
+    CDinausor(int x, int y);
     void tell();
-
 };
 
 #endif
